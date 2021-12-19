95d2121c0001f95608586000dda5955bd70c2c96
BeckyPollard <hello@beckypollard.com>
2021-12-19 12:52:55 -0500

    Weather themes WIP

--- src/scripts/App.tsx
+++ src/scripts/App.tsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import SunCalc from 'suncalc';
 import CurrentWeather from './components/CurrentWeather';
 import Header from './components/Header';
 
 function App() {
   const [weather, setWeather] = useState<any>();
+  const [weatherTime, setWeatherTime] = useState<Date>();
   const [loading, setLoading] = useState<boolean>(true);
   const [theme, setTheme] = useState<string>('theme-day-clear');
+  // const [daytime, setDaytime] = useState<boolean>(true);
 
   const getCurrentWeather = () => {
     const key = process.env.API_KEY;
@@ -14,45 +17,52 @@ function App() {
       .then(response => response.json())
       .then(data => {
         setWeather(data.observations[0]);
+        setWeatherTime(new Date(data.observations[0].epoch * 1000))
       })
       .catch((error) => {
         console.error(error);
       }
     );
   }
+
   useEffect(() => {
     getCurrentWeather();
+
     setLoading(false)
   }, []);
-  console.log('🌦 Weather API from Wunderground:', weather);
-
+  
   if (loading || !weather) {
     return (
-      <div className={`theme ${theme}`}>
-        <div className='wrapper'>
-          <Header/>
-          <CurrentWeather/>
-        </div>
+      <div className='wrapper'>
+        <Header/>
+        <CurrentWeather/>
       </div>
     );
   };
+  console.log('🌦 Weather API from Wunderground:', weather);
 
-  const time = new Date(weather.epoch * 1000);
+  
+  const setSun = (time: Date) => {
+    const sun = SunCalc.getTimes(time, weather.lat, weather.lon)
+    console.log('sunset = ', sun.sunset);
+    console.log('sunrise = ', sun.sunrise);
+    console.log('current time = ', time);
+  }
 
-  console.log(setTheme);
+
+
+  console.info('shuttup typescript', setTheme, theme, setSun); //quiet typescript
 
   return (
-    <div className={`theme ${theme}`}>
-      <div className='wrapper'>
-        <Header
-          station={weather.stationID}
-          time={time}
-        />
-        <CurrentWeather 
-          time={time}
-          weather={weather}
-        />
-      </div>
+    <div className='wrapper'>
+      <Header
+        station={weather.stationID}
+        time={weatherTime}
+      />
+      <CurrentWeather 
+        time={weatherTime}
+        weather={weather}
+      />
     </div>
   );
 };
